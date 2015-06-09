221d5f2edf3437731610d8c3e976917eeca5757a
funnycircus <funnycircus@SD2-funnycircus.ptc-nec.com.tw>
2015-06-09 21:55:31 +0800

    first commit

---
+++ Retangle/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 矩形
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //GET Retangle x and y
+            int rect_x = int.Parse(this.textBox1.Text);
+            int rect_y = int.Parse(this.textBox2.Text);
+
+            if (rect_x <= 0)
+            {
+                rect_x = 0;
+                this.textBox1.Text = "0";
+            }
+            else if (rect_x > 300)
+            {
+                rect_x = 300;
+                this.textBox1.Text = "300";
+            }
+
+            if (rect_y <= 0)
+            {
+                rect_y = 0;
+                this.textBox2.Text = "0";
+            }
+            else if(rect_y > 180)
+            {
+                rect_y = 180;
+                this.textBox2.Text = "180";
+            }
+
+            Graph drawer = new Graph(rect_x, rect_y);
+            
+            //draw Retangle on PictureBox
+            drawer.DrawRectangleRectangle(e.Graphics, e);
+        }
+    }
+}
---
+++ Retangle/GRAPH.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 矩形
+{
+    class Graph
+    {
+        private int rect_x = 0;
+        private int rect_y = 0;
+
+        public Graph(int x, int y) {
+            if (x <= 0)
+            {
+                this.rect_x = 0;
+            }
+            else if (x > 300)
+            {
+                this.rect_x = 300;
+            }
+            else 
+            {
+                this.rect_x = x;
+            }
+
+            if (y <= 0)
+            {
+                this.rect_y = 0;
+            }
+            else if (y > 180) 
+            {
+                this.rect_y = 180;
+            }
+            else
+            {
+                this.rect_y = y;
+            }
+        }
+
+        
+        public void DrawRectangleRectangle(System.Drawing.Graphics graphics, System.Windows.Forms.PaintEventArgs e)
+        {
+
+            // Create pen.
+            System.Drawing.Pen blackPen = new System.Drawing.Pen(System.Drawing.Color.Black, 3);
+
+            // Create rectangle.
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, this.rect_x, this.rect_y);
+
+            // Draw rectangle to screen.
+            e.Graphics.DrawRectangle(blackPen, rect);
+        }
+
+    }
+}
